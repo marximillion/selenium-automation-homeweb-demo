83602046e32e79840c5045b9775ecb06362b1ca4
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-01-30 16:52:10 -0700

    TESTING - Public Landing

--- src/tests/Smoke.ts
+++ src/tests/Smoke.ts
@@ -5,10 +5,11 @@
  */
 import { Browser, Builder } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome';
-import { Footer } from './Footer';
+import { Footer } from './general/Footer';
 import { generateReport } from '../common/Utility';
-import { Header } from './Header';
+import { Header } from './general/Header';
 import { HOMEWEB_LANDING_URL_EN, HOMEWEB_LANDING_URL_FR, LANGUAGE } from '../common/Constants';
+import { PublicLanding } from './public/PublicLanding';
 
 /**
  * Smoke - Test
@@ -44,12 +45,16 @@ async function Smoke() {
         await chromeDriver.get(TARGET_URL_EN)
 
         // 3.2: Header
-        const header_en = new Header(LANGUAGE.ENGLISH, chromeDriver, TARGET_URL_EN, windowHandle);
-        await header_en.runTests();
+        // const header_en = new Header(LANGUAGE.ENGLISH, chromeDriver, TARGET_URL_EN, windowHandle);
+        // await header_en.runTests();
 
         // 3.3: Footer
-        const footer_en = new Footer(LANGUAGE.ENGLISH, chromeDriver, TARGET_URL_EN, windowHandle);
-        await footer_en.runTests();
+        // const footer_en = new Footer(LANGUAGE.ENGLISH, chromeDriver, TARGET_URL_EN, windowHandle);
+        // await footer_en.runTests();
+
+        // 3.4: Public - Landing
+        const public_landing_en = new PublicLanding(LANGUAGE.ENGLISH, chromeDriver, TARGET_URL_EN, windowHandle);
+        await public_landing_en.runTests();
 
         /**
          * 4: FR Tests
@@ -58,20 +63,26 @@ async function Smoke() {
         await chromeDriver.get(TARGET_URL_FR)
 
         // 4.2: Header
-        const header_fr = new Header(LANGUAGE.FRENCH, chromeDriver, TARGET_URL_FR, windowHandle);
-        await header_fr.runTests();
+        // const header_fr = new Header(LANGUAGE.FRENCH, chromeDriver, TARGET_URL_FR, windowHandle);
+        // await header_fr.runTests();
 
         // 4.3: Footer
-        const footer_fr = new Footer(LANGUAGE.FRENCH, chromeDriver, TARGET_URL_FR, windowHandle);
-        await footer_fr.runTests();
+        // const footer_fr = new Footer(LANGUAGE.FRENCH, chromeDriver, TARGET_URL_FR, windowHandle);
+        // await footer_fr.runTests();
+
+        // 4.4: Public - Landing
+        const public_landing_fr = new PublicLanding(LANGUAGE.FRENCH, chromeDriver, TARGET_URL_FR, windowHandle);
+        await public_landing_fr.runTests();
 
         /**
          * 5: Generate Report
          */
-        const total = header_en.testTotal + header_fr.testTotal + footer_en.testTotal + footer_fr.testTotal;
-        const pass = header_en.passed + header_fr.passed + footer_en.passed + footer_fr.passed;
-        const fail = header_en.failed + header_fr.failed + footer_en.failed + footer_fr.failed;
-        console.log(generateReport(total, pass, fail))
+        // const total = header_en.testTotal + header_fr.testTotal + footer_en.testTotal + footer_fr.testTotal + public_landing_en.testTotal + public_landing_fr.testTotal;
+        // const pass = header_en.passed + header_fr.passed + footer_en.passed + footer_fr.passed + public_landing_en.passed + public_landing_fr.passed;
+        // const fail = header_en.failed + header_fr.failed + footer_en.failed + footer_fr.failed + public_landing_en.failed + public_landing_fr.failed;
+        // console.log(generateReport(total, pass, fail));
+        console.log(generateReport(public_landing_en.testTotal, public_landing_en.passed, public_landing_en.failed));
+        console.log(generateReport(public_landing_fr.testTotal, public_landing_fr.passed, public_landing_fr.failed));
     }
     catch (error: any) {
         console.log('smoke->onFailure\n', error);
