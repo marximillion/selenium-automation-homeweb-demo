5a2367808a51f530319576e69e4f7f2448ad3acd
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-01-30 09:45:53 -0700

    Remove Header constants

--- src/Constants.ts
+++ src/Constants.ts
@@ -1,45 +1,31 @@
 /* MDG 2026 */
 
+import { translate } from './Utility';
+
 /**
  * General
  */
-export const CLICK_DELAY = 1500;
+export const CLICK_DELAY = 1000;
+export const HOMEWEB_DOMAIN = 'https://homeweb.ca';
+export const HOMEWOOD_API_DOMAIN = 'https://api.homewoodhealth.io';
 export const HOMEWEB_LANDING_URL_EN = 'https://homeweb.ca/en';
 export const HOMEWEB_LANDING_URL_FR = 'https://homeweb.ca/fr';
-
-/**
- * Header - Labels
- */
-const LABEL_HOMEWOOD_LOGO_EN = '[aria-label="Homewood Logo"]';
-const LABEL_LANGUAGE_TOGGLE_EN = '[aria-label="Toggle language to FR"]';
-const LABEL_SIGN_IN_BUTTON_EN = '[aria-label="Sign In"]';
-
-const LABEL_HOMEWOOD_LOGO_FR = '[aria-label="Logo Homewood"]';
-const LABEL_LANGUAGE_TOGGLE_FR = '[aria-label="Basculer la langue en EN"]';
-const LABEL_SIGN_IN_BUTTON_FR = '[aria-label="Se connecter"]';
 
 export const enum LANGUAGE {
     ENGLISH = 'en',
     FRENCH = 'fr'
 }
 
-export const enum COMPONENT {
-    HEADER = 'header',
+export const enum TAG {
+    HEADER= 'header',
     FOOTER = 'footer',
-}
-
-export const enum HEADER {
-    EN_LOGO = LABEL_HOMEWOOD_LOGO_EN,
-    EN_TOGGLE = LABEL_LANGUAGE_TOGGLE_EN,
-    EN_BUTTON = LABEL_SIGN_IN_BUTTON_EN,
-    FR_LOGO = LABEL_HOMEWOOD_LOGO_FR,
-    FR_TOGGLE = LABEL_LANGUAGE_TOGGLE_FR,
-    FR_BUTTON = LABEL_SIGN_IN_BUTTON_FR
+    MAIN = 'main',
 }
 
 /**
  * Footer - Labels
  */
+// REVIEW this and try to use a different locator since the href should be used for validation
 const LABEL_HOMEWOOD_ABOUT_EN = 'a[href="/en/about"]';
 const LABEL_HOMEWOOD_TERMS_EN = 'a[href="/en/terms-of-service"]';
 const LABEL_HOMEWOOD_PRIVACY_EN = 'a[href="/en/privacy-policy"]';
