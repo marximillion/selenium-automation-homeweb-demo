d230e23e222c3b153a82a49b583f89275fdeb62b
Mark De Guzman <mdguzman@homewoodhealth.com>
2026-01-30 16:51:18 -0700

    Update Constants

--- src/common/Constants.ts
+++ src/common/Constants.ts
@@ -6,7 +6,8 @@
 export const CLICK_DELAY = 500;
 export const HOMEWEB_DOMAIN = 'https://homeweb.ca';
 export const HOMEWOOD_DOMAIN = 'https://homewoodhealth.com';
-export const HOMEWOOD_API_DOMAIN = 'https://api.homewoodhealth.io';
+export const QUANTUM_API_DOMAIN = 'https://api.homewoodhealth.io';
+export const IDENTITY_API_DOMAIN = 'https://i7.homewoodhealth.com'
 export const HOMEWEB_LANDING_URL_EN = 'https://homeweb.ca/en';
 export const HOMEWEB_LANDING_URL_FR = 'https://homeweb.ca/fr';
 
@@ -19,4 +20,5 @@ export const enum TAG {
     HEADER= 'header',
     FOOTER = 'footer',
     MAIN = 'main',
+    PUBLIC_LANDING = 'public-landing',
 }
